30a007171e7b5ed648f2f2012df70307d1b298e4
btok-vfx <128278229+btok-vfx@users.noreply.github.com>
2023-03-23 14:14:45 +0530

    Create contact.php

---
+++ forms/contact.php
@@ -0,0 +1,26 @@
+<?php
+  // Get the form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  // Set the recipient email address
+  $to = 'bensonpr100@gmail.com';
+
+  // Set the email subject
+  $subject = 'New contact form submission';
+
+  // Build the email content
+  $email_content = "Name: $name\n";
+  $email_content .= "Email: $email\n\n";
+  $email_content .= "Message:\n$message\n";
+
+  // Build the email headers
+  $email_headers = "From: $name <$email>";
+
+  // Send the email
+  mail($to, $subject, $email_content, $email_headers);
+
+  // Redirect to the thank you page
+  header('Location: btok-efx.github.io');
+?>
